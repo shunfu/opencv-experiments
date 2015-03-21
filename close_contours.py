4761a51c7552b84993c75b8b7f6a253dc29aa279
Shunhua Fu <shunhua.fu@gmail.com>
2015-03-21 14:37:10 -0500

    Completed experiments for now

--- close_contours.py
+++ close_contours.py
@@ -1,6 +1,23 @@
 import numpy as np
 import cv2
 
+def get_bounds(contours):
+    x_max = 0
+    x_min = frame.shape[0]
+    y_max = 0
+    y_min = frame.shape[1]
+    for cnt in contours:
+        for coord in cnt:
+            if coord[0][0] > x_max:
+                x_max = coord[0][0]
+            if coord[0][0] < x_min:
+                x_min = coord[0][0]
+            if coord[0][1] > y_max:
+                y_max = coord[0][1]
+            if coord[0][1] < y_min:
+                y_min = coord[0][1]
+    return x_min, x_max, y_min, y_max
+
 def draw_full_contours(frame):
     # draw contours on full image
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -13,30 +30,15 @@ def draw_subtracted_contours(frame):
     fgmask = fgbg.apply(frame)
     _, contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # cv2.drawContours(frame, contours, -1, (0, 255, 0), 20) # fills in full body shape
-    cv2.fillPoly(frame, contours, (0, 255, 0)) # looks better
+    cv2.fillPoly(frame, contours, (200, 200, 200)) # looks better
 
 def draw_bounding_box(frame):
     # draw rect that holds the subject of the image
-
     fgmask = fgbg.apply(frame)
     _, contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # cv2.fillPoly(frame, contours, (0, 255, 0)) # looks better
 
-    x_max = 0
-    x_min = frame.shape[0]
-    y_max = 0
-    y_min = frame.shape[1]
-    for cnt in contours:
-        for coord in cnt:
-            if coord[0][0] > x_max:
-                x_max = coord[0][0]
-            if coord[0][0] < x_min:
-                x_min = coord[0][0]
-            if coord[0][1] > y_max:
-                y_max = coord[0][1]
-            if coord[0][1] < y_min:
-                y_min = coord[0][1]
-
+    x_min, x_max, y_min, y_max = get_bounds(contours)
     # print "(%d : %d, %d : %d)"%(x_min, x_max, y_min, y_max)
     cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (0, 255, 0))
 
@@ -44,21 +46,8 @@ def draw_canny_edge(frame):
     # draw canny edge of the subject of the image
     fgmask = fgbg.apply(frame)
     _, contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    x_max = 0
-    x_min = frame.shape[0]
-    y_max = 0
-    y_min = frame.shape[1]
-    for cnt in contours:
-        for coord in cnt:
-            if coord[0][0] > x_max:
-                x_max = coord[0][0]
-            if coord[0][0] < x_min:
-                x_min = coord[0][0]
-            if coord[0][1] > y_max:
-                y_max = coord[0][1]
-            if coord[0][1] < y_min:
-                y_min = coord[0][1]
+    
+    x_min, x_max, y_min, y_max = get_bounds(contours)
 
     edges = cv2.Canny(frame, 100, 200)
     _, contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -68,8 +57,11 @@ def draw_canny_edge(frame):
     cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (255, 255, 255))
 
 def draw_silhouette(frame):
-    # TODO fill it in!!
-    pass
+    # draw silhouette of the subject
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 127, 255, 0)
+    _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.fillPoly(frame, contours, (0, 215, 25))
 
 camera = cv2.VideoCapture(0)
 # cv2.namedWindow('frame', cv2.WND_PROP_FULLSCREEN)
@@ -87,7 +79,11 @@ while True:
     # draw_subtracted_contours(frame)
     # draw_bounding_box(frame)
     # draw_canny_edge(frame)
+    # draw_silhouette(frame)
+
+    # fills in silhouette and then draws in forces
     draw_silhouette(frame)
+    draw_subtracted_contours(frame)
 
     # display the frame
     cv2.imshow('frame', frame)
